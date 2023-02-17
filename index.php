abf65478ccf33e0caee0c8d1358cfdbf65d9ce84
achrafAR <achraf.alrachini@gmail.com>
2023-02-17 16:35:11 +0200

    Strings Concatenation

--- index.php
+++ index.php
@@ -8,18 +8,33 @@
 </head>
 <body>
 <?php
-    $name = "Achraf";
-    $email = "achraf.alrachini@gmail.com";
-    echo "My name is $name and my email is $email";
-    $height = 180;
-    $width = 50;
-    $area = $width*$height;
+    // $name = "Achraf";
+    // $email = "achraf.alrachini@gmail.com";
+    // echo "My name is $name and my email is $email";
+    // $height = 180;
+    // $width = 50;
+    // $area = $width*$height;
+    // echo "<br>";
+    // echo $area;
+
+    $name = "achraf";
+    $position = "web developer";
+    $github = "https://github.com/achrafAR";
+    // Using double quotation " and the .
+    echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$github_url."";
     echo "<br>";
-    echo $area;
+    echo "Hello, I'm $name, I'm a $position please check my github link $github_url";
+    echo "<br>";
+    echo 'Hello, I\'m '.$name.', I\'m a '.$position.' please check my github link '.$github_url.'';
+    
+
 
 ?>
 </body>
 </html>
+
+
+
 
 
 
