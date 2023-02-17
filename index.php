00a0b032001f9139e55cd39b1fee95e575eab051
achrafAR <achraf.alrachini@gmail.com>
2023-02-17 18:26:45 +0200

    Data types - lvl 1

--- index.php
+++ index.php
@@ -8,25 +8,52 @@
 </head>
 <body>
 <?php
-    // $name = "Achraf";
-    // $email = "achraf.alrachini@gmail.com";
-    // echo "My name is $name and my email is $email";
-    // $height = 180;
-    // $width = 50;
-    // $area = $width*$height;
-    // echo "<br>";
-    // echo $area;
 
+    echo "<h1>Step 1</h1>";
+    $name = "Achraf";
+    $email = "achraf.alrachini@gmail.com";
+    echo "My name is $name and my email is $email";
+    $height = 180;
+    $width = 50;
+    $area = $width*$height;
+    echo "<br>";
+    echo $area;
+    echo "<br>";    echo "<br>";
+    echo "<br>";
+
+
+    echo "<h1>Step 2</h1>";
     $name = "achraf";
     $position = "web developer";
     $github = "https://github.com/achrafAR";
-    // Using double quotation " and the .
     echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$github_url."";
     echo "<br>";
     echo "Hello, I'm $name, I'm a $position please check my github link $github_url";
     echo "<br>";
-    echo 'Hello, I\'m '.$name.', I\'m a '.$position.' please check my github link '.$github_url.'';
+    echo 'Hello, I\'m '.$name.', I\'m a '.$position.' please check my github link '.$github_url.'';    echo "<br>";    echo "<br>";     echo "<br>";
+
+
+
     
+
+    echo "<h1>Step 5</h1>";
+    $course_name = "MERN Stack";
+    $enrolled_students = 20;
+    $price = 12.33;
+    $on_discount = "Yes / No";
+
+    echo "Course title: "  .$course_name.  "<br>";
+    echo "Enrolled Students: " .$enrolled_students. "<br>";
+    echo "Course price: " . $price . " $ USD<br>";
+    echo "Course on discount: " . $on_discount . "<br>";     echo "<br>";     echo "<br>";
+
+
+    echo "<h1>Step 6</h1>";
+
+
+
+
+
 
 
 ?>
